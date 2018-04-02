8d1ab4a573c8c080a87b4d325da544a72d28b9ca
Mark Xie <xiaofeng02111@gmail.com>
2018-04-01 21:05:43 -0400

    Added scroll

--- src/Components/Home/Download/DownloadTable/index.js
+++ src/Components/Home/Download/DownloadTable/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Grid, Image, Button } from "semantic-ui-react";
 import DownloadForm from "../DownloadForm";
 import data from "../../../../data";
-
+import ScrollableAnchor from "react-scrollable-anchor";
 export default class DownloadTable extends Component {
   render() {
     const {
@@ -12,58 +12,60 @@ export default class DownloadTable extends Component {
       changeHandler
     } = this.props;
     return (
-      <div id="selection">
-        <Grid stackable>
-          <Grid.Row centered>
-            <Grid.Column>
-              <Image src={frameworkLogo} centered />
-            </Grid.Column>
-          </Grid.Row>
+      <ScrollableAnchor id={"selection"}>
+        <div>
+          <Grid stackable>
+            <Grid.Row centered>
+              <Grid.Column>
+                <Image src={frameworkLogo} centered />
+              </Grid.Column>
+            </Grid.Row>
 
-          <Grid.Row columns={16} textAlign="left">
-            <Grid.Column width={2} />
-            <Grid.Column width={3}>
-              <DownloadForm
-                name="Common"
-                data={data.common}
-                handler={changeHandler}
-              />
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <DownloadForm
-                name="Cask"
-                data={data.cask}
-                handler={changeHandler}
-              />
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <DownloadForm
-                name="Framework"
-                data={data[`${frameworkName}`]}
-                handler={changeHandler}
-              />
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <DownloadForm
-                name="VS Code Extension"
-                data={data.vs_extension}
-                handler={changeHandler}
-              />
-            </Grid.Column>
-            <Grid.Column width={2} />
-          </Grid.Row>
-          <Grid.Row centered columns={5}>
-            <Button
-              primary
-              size="massive"
-              style={{ margin: "2em" }}
-              onClick={downloadHandler}
-            >
-              Download
-            </Button>
-          </Grid.Row>
-        </Grid>
-      </div>
+            <Grid.Row columns={16} textAlign="left">
+              <Grid.Column width={2} />
+              <Grid.Column width={3}>
+                <DownloadForm
+                  name="Common"
+                  data={data.common}
+                  handler={changeHandler}
+                />
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <DownloadForm
+                  name="Cask"
+                  data={data.cask}
+                  handler={changeHandler}
+                />
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <DownloadForm
+                  name="Framework"
+                  data={data[`${frameworkName}`]}
+                  handler={changeHandler}
+                />
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <DownloadForm
+                  name="VS Code Extension"
+                  data={data.vs_extension}
+                  handler={changeHandler}
+                />
+              </Grid.Column>
+              <Grid.Column width={2} />
+            </Grid.Row>
+            <Grid.Row centered columns={5}>
+              <Button
+                primary
+                size="massive"
+                style={{ margin: "2em" }}
+                onClick={downloadHandler}
+              >
+                Download
+              </Button>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </ScrollableAnchor>
     );
   }
 }
