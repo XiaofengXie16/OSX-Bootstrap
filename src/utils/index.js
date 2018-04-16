d20241c983fef275b16385102bcbdf434426a836
Mark Xie <xiaofeng02111@gmail.com>
2018-04-16 14:41:49 -0400

    Added test files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,32 +1,48 @@
-import { template } from "../template";
-import data from "../data";
 import fileDownload from "js-file-download";
 import JSZip from "jszip";
-export const categoryBuilder = (array, identifier, prefix = "") => {
-  let result = [];
-  for (let key in array) {
-    if (array[key].category === identifier) {
-      result.push(prefix + array[key].value);
-    }
-  }
-  return result.join("\n");
-};
+import data from "../data";
+import { template } from "../template";
 
+/**
+ * This function converts a nested pacakge data object to an array of objects
+ * @param {Object} data
+ */
 export const transform = data => {
-  let transform = [];
-  for (let key in data) {
-    transform = [...transform, ...data[key]];
-  }
-  return transform;
+  const reducer = (accumulator, cur) => [...accumulator, ...cur];
+  return Object.keys(data)
+    .map(item => data[item])
+    .reduce(reducer, []);
 };
 
+/**
+ *  This function filters the package data based on the user selection on the UI
+ * @param {array} data  - an array of package data object
+ * @param {array} state - react state object that contains user's selection
+ */
 export const selectionFilter = (data, state) => {
-  const result = [];
-  for (let o in data) {
-    const key = data[o].identifier;
-    if (state[key]) result.push(data[o]);
-  }
-  return result;
+  const reducer = (accumulator, cur) => {
+    accumulator.push(cur);
+    return accumulator;
+  };
+  return data.filter(item => state[item.identifier]).reduce(reducer, []);
+};
+
+/**
+ * This function builds the linux template string based on the package category
+ * @param {array} data        - an array of pacakge object
+ * @param {string} identifier - category
+ * @param {string} prefix     - linux command prefix
+ */
+export const categoryBuilder = (data, identifier, prefix = "") => {
+  const reducer = (accumulator, cur) => {
+    accumulator.push(cur);
+    return accumulator;
+  };
+  return data
+    .filter(item => item.category === identifier)
+    .map(item => prefix + item.value)
+    .reduce(reducer, [])
+    .join("\n");
 };
 
 const COMMON_CATEGORY = "common";
@@ -36,11 +52,15 @@ const NPM_CATEGORY = "npm";
 const EXTENSION_CATEGORY = "extension";
 const transformed = transform(data);
 
+/**
+ * This functions creates the installation script and download the script to user's local system
+ * @param {array} data
+ */
 export const downloadHelper = async data => {
   const zip = new JSZip();
   const result = selectionFilter(transformed, data);
-  const common = categoryBuilder(result, COMMON_CATEGORY);
 
+  const common = categoryBuilder(result, COMMON_CATEGORY);
   const cask = categoryBuilder(result, CASK_CATEGORY);
   const framework = categoryBuilder(
     result,
